248c4253ee8fc4276e93b15ad29f813cffc96ab0
Konstantin Kolomiec <77701872+Konstantin-Kolom@users.noreply.github.com>
2021-08-26 01:30:19 +0300

    1

--- src/App.js
+++ src/App.js
@@ -1,25 +1,31 @@
-import logo from './logo.svg';
-import './App.css';
+import Profile from './components/Profile';
+import Statistics from './components/Statistics';
+import user from './json/user.json';
+import statistics from './json/statistical-data.json';
 
-function App() {
+// const stat1 = statistic[0];
+
+export default function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <div>
+        <Profile
+          avatar={user.avatar}
+          name={user.name}
+          tag={user.tag}
+          location={user.location}
+          stats={user.stats}
+        />
+      </div>
+      <div>
+        {statistics.map(statistic => (
+          <Statistics
+            key={statistic.id}
+            label={statistic.label}
+            percentage={statistic.percentage}
+          />
+        ))}
+      </div>
     </div>
   );
 }
-
-export default App;
